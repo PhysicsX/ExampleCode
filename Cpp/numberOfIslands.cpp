7ec397b0ccebcadbab93e733d39dc55d7cfcf1bc
ulas dikme <ulasdikme@gmail.com>
2020-05-20 00:54:08 +0300

    Update vector usage

--- Cpp/numberOfIslands.cpp
+++ Cpp/numberOfIslands.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 // g++ -std=c++14 numberOfIslands.cpp -o numberOfIslands
@@ -10,43 +9,56 @@ using namespace std;
 class Solution
 {
 	public:
-		int numOfIsland(vector<vector<char>> grid)
+		int numOfIsland(vector<vector<char>> vec)
 		{
-			int m = grid.size(), n = m ? grid[0].size() : 0,
-			islands = 0;
-			int offsets[] = {0,1,0,-1,0};
 			
-			for(int i=0; i < m; i++)
-			{
-				for(int j=0; j < n; j++)
-				{
-					if(grid[i][j] == '1')
-					{
-						islands++; // increase the number of island when encounter with '1'
-						grid[i][j] = '0'; // assign zero to explored coordinate
-						queue<pair<int,int>> todo;
-						todo.push({i,j});
-						
-						while(!todo.empty())
-						{
-							pair<int,int> p = todo.front();
-							todo.pop();
-							for(int k=0; k<4; k++)
-							{
-								int r = p.first + offsets[k], c = p.second + offsets[k+1];
-								if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1')
-								{
-									grid[r][c] = '0';
-									todo.push({r,c});
-								}	
-							}	
-						}	
-					}
+			int result = 0; // number of islands
+			if(vec.size() == 0)
+				return result;
+			
+			int numberOfRows = vec.size();
+			int numberOfColumns = vec.at(0).size();
+			vector<int> neighborCorr = {0,1,0,-1,0};
 
+			for(int i=0; i<numberOfRows; i++)
+			{
+				for(int j=0; j<numberOfColumns; j++)
+				{
+					if(vec.at(i).at(j) == '1')
+					{
+						result++;
+						queue<pair<int,int>> que;
+						que.push({i,j});
+						vec.at(i).at(j) = '0';
+
+						while(!que.empty())
+						{
+							auto p = que.front();
+							que.pop();
+
+							for(int k=0; k<neighborCorr.size(); k++)
+							{
+							
+								int x = p.first + neighborCorr[k];
+								int y = p.second + neighborCorr[k+1];
+
+								if(x>=0 && x<numberOfRows && y>=0 && y<numberOfColumns)
+								{
+									if(vec.at(x).at(y) == '1')
+									{
+										vec.at(x).at(y) = '0';
+										que.push({x,y});
+									}
+								}
+							}
+						
+						}
+					}
 				}
 			
 			}
-			return islands;
+
+			return result;
 		}
 }; // end of the Solution Class		
 int main()
