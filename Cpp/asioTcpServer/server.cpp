ed0c37b439e213d39705822a4aef81f43e699ce5
ulas dikme <ulasdikme@gmail.com>
2020-06-20 13:05:06 +0100

    Correct namespaces for synchron server

--- Cpp/asioTcpServer/server.cpp
+++ Cpp/asioTcpServer/server.cpp
@@ -4,17 +4,10 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/strand.hpp>
+
 
 //using namespace boost::asio
 //using ip::tcp
-
-using std::string;
-using std::cout;
-using std::endl;
 
 int main(int argc, char* argv[]) 
 {
@@ -28,20 +21,18 @@ int main(int argc, char* argv[])
 	auto const address = boost::asio::ip::make_address(argv[1]);
 	auto const port = std::atoi(argv[2]);
 
-	
-
 	boost::asio::io_service io_service;
 	//listen for new connection
 	boost::system::error_code ec;
-      	boost::asio::ip::tcp::acceptor acceptor_(io_service, boost::asio::ip::tcp::endpoint(address, port )); 
+	boost::asio::ip::tcp::acceptor acceptor_(io_service, boost::asio::ip::tcp::endpoint(address, port )); 
 	boost::asio::ip::tcp::socket socket_(io_service);//socket creation	
 	acceptor_.accept(socket_);//waiting for connection
 	//read operation
 	boost::asio::streambuf buf;
 	boost::asio::read_until( socket_, buf, "\n",ec);
 	if(ec){std::cout<<ec.message()<<std::endl; return 1;}	
-	string data = boost::asio::buffer_cast<const char*>(buf.data());
-	cout << data << endl;
+	std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+	std::cout << data << std::endl;
 	//write operation
 	boost::asio::write( socket_, boost::asio::buffer(data) ,ec);
 	if(ec){std::cout<<ec.message()<<std::endl; return 1;}		
