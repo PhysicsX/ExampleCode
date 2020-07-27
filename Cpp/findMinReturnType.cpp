3ed2e49ea713ff07226bf685c9e002a59224e28c
ulasdikme <ulasdikme@gmail.com>
2020-07-27 22:20:31 +0300

    decltype template auto return function example

---
+++ Cpp/findMinReturnType.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+template <typename A, typename B>
+auto findMin(A a, B b) -> decltype( (a < b) ? a: b)
+{
+        return (a < b) ? a : b;
+}
+
+int main()
+{
+
+    std::cout<<findMin(3.14, 4)<<std::endl;
+    
+    //check the return type is dobule or not
+    std::cout << std::is_same<double, decltype(findMin(3.14, 4))>::value<<std::endl;
+    std::cout << std::is_same<int, decltype(findMin(3.14, 4))>::value<<std::endl;
+
+    return 0;
+}
