2886f7ff7824ceb6f514db5c838bc24186753e43
ulasdikme <ulasdikme@gmail.com>
2020-07-28 17:01:41 +0300

    update deduction type

--- Cpp/findMinReturnType.cpp
+++ Cpp/findMinReturnType.cpp
@@ -4,9 +4,14 @@
 using namespace std;
 
 template <typename A, typename B>
-auto findMin(A a, B b) -> decltype( (a < b) ? a: b)
+auto findMin(A a, B b) -> decltype( (a < b) ? a: b) // decltype is not necessary for c++14
 {
         return (a < b) ? a : b;
+}
+
+auto foo(int a, double b)
+{
+  return (a < b) ? a : b;
 }
 
 int main()
