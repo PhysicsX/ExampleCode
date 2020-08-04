63f0793a38c4f4321870531b08c32afab228c2e7
ulasdikme <ulasdikme@gmail.com>
2020-08-04 15:37:32 +0300

    faster response update

---
+++ Py/regressinTf.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+#create data set
+x = np.linspace(0.0, 10.0, 1000000)
+y = (0.7 * x) + 8 + np.random.randn(len(x))
+x_df = pd.DataFrame(data=x, columns=['X'])
+print(x_df)
+y_df = pd.DataFrame(data=y, columns=['Y'])
+print(y_df)
+
+collect_data = pd.concat([x_df, y_df], axis=1)
+print(collect_data)
+collect_data.sample(300).plot(kind='scatter', x='X', y='Y')
+#plt.show()
+
+#create model
+step_size=10
+m = tf.Variable(0.21)
+b = tf.Variable(0.71)
+x_ph = tf.placeholder(tf.float32)
+y_ph = tf.placeholder(tf.float32)
+model_eq = tf.add(tf.multiply(m, x_ph),b) #model_eq = m*x_ph+b
+train_model = tf.train.GradientDescentOptimizer(learning_rate=1e-5).minimize(tf.reduce_mean(tf.square((y_ph)-model_eq)))
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    train_step = 100000
+    for i in range(train_step):
+        index = np.random.randint(len(x),size=step_size)
+        feed = {x_ph: x[index], y_ph:y[index]}
+        sess.run(train_model, feed_dict=feed)
+    result_m, result_b = sess.run([m,b])
+
+result_y = x * result_m + result_b
+plt.plot(x, result_y, 'r')
+plt.show()
