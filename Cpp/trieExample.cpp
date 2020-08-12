0414683e767539d66d5e76213ce491475d0c5d89
ulasdikme <ulasdikme@gmail.com>
2020-08-12 21:08:49 +0300

    print trie tree

--- Cpp/trieExample.cpp
+++ Cpp/trieExample.cpp
@@ -2,6 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ * 
+ * Trie is an efficient information retrieval data structure. 
+ * If keys are stored in binary search tree, a well balanced BST
+ * will need time proportional to M * log N, where M is maximum string
+ * length and N is number of keys in tree. Using Trie, we can search the key
+ * O(M) time. However the penalty is on Trie storage requirements.
+ * 
+ * */
+
 const int ALPHABET_SIZE = 26;
 
 struct TrieNode
@@ -40,6 +50,26 @@ void insert(struct TrieNode *root, string key)
 }
 
 
+void display(struct TrieNode* root, char str[], int level)
+{
+    if(root->isEndOfWord != false)
+    {
+        str[level] = '\0';
+        cout<<str<<endl;
+    }
+    
+    for(int i=0; i < ALPHABET_SIZE; i++)    
+    {
+        
+        if(root->children[i])
+        {
+            str[level] = i + 'a';
+            display(root->children[i], str, level + 1);
+        }
+    }
+    
+}
+
 bool search(struct TrieNode *root, string key)
 {
         struct TrieNode *pCrawl = root;
@@ -70,9 +100,10 @@ int main()
     
     
     search(root, "the") ? cout<<"Yes\n" : cout<<"No\n";
-    
+    search(root, "there") ? cout<<"Yes\n" : cout<<"No\n";
     search(root, "these") ? cout<<"Yes\n" : cout<<"No\n";
-    
+    char str[20];
+    display(root, str, 0);
     
     return 0;
 }
