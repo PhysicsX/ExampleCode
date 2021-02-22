2b8c104db34e95b17fabdbd8e895a9d08be7f15c
PhysicsX <ulasdikme@gmail.com>
2021-02-22 23:00:48 +0100

    Picamera example over gstreamer

---
+++ Py/cameraJetsonNanoGst.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import cv2
+import gi
+
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import Gst, GstRtspServer, GObject
+
+dispW=640
+dispH=480
+flip=2
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+
+class SensorFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self, **properties):
+        super(SensorFactory, self).__init__(**properties)
+	#cam= cv2.VideoCapture(camSet)
+        self.cap = cv2.VideoCapture(camSet) # 0 for usb camera
+        self.number_frames = 0
+        self.fps = 30
+        self.duration = 1 / self.fps * Gst.SECOND  # duration of a frame in nanoseconds
+        self.launch_string = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
+                             'caps=video/x-raw,format=BGR,width=640,height=480,framerate={}/1 ' \
+                             '! videoconvert ! video/x-raw,format=I420 ' \
+                             '! x264enc speed-preset=ultrafast tune=zerolatency ' \
+                             '! rtph264pay config-interval=1 name=pay0 pt=96'.format(self.fps)
+
+    def on_need_data(self, src, lenght):
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                data = frame.tostring()
+                buf = Gst.Buffer.new_allocate(None, len(data), None)
+                buf.fill(0, data)
+                buf.duration = self.duration
+                timestamp = self.number_frames * self.duration
+                buf.pts = buf.dts = int(timestamp)
+                buf.offset = timestamp
+                self.number_frames += 1
+                retval = src.emit('push-buffer', buf)
+                print('pushed buffer, frame {}, duration {} ns, durations {} s'.format(self.number_frames,
+                                                                                       self.duration,
+                                                                                       self.duration / Gst.SECOND))
+                if retval != Gst.FlowReturn.OK:
+                    print(retval)
+
+    def do_create_element(self, url):
+        return Gst.parse_launch(self.launch_string)
+
+    def do_configure(self, rtsp_media):
+        self.number_frames = 0
+        appsrc = rtsp_media.get_element().get_child_by_name('source')
+        appsrc.connect('need-data', self.on_need_data)
+
+
+class GstServer(GstRtspServer.RTSPServer):
+    def __init__(self, **properties):
+        super(GstServer, self).__init__(**properties)
+        self.factory = SensorFactory()
+        self.factory.set_shared(True)
+        self.get_mount_points().add_factory("/test", self.factory)
+        self.attach(None)
+
+
+GObject.threads_init()
+Gst.init(None)
+
+server = GstServer()
+
+loop = GObject.MainLoop()
+loop.run()
