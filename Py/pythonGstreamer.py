5f190fddb5f1bf68bd514d58878b39ce56587125
PhysicsX <ulasdikme@gmail.com>
2021-04-19 23:25:11 +0000

    Gstreamer python example for nvidia jetson

---
+++ Py/pythonGstreamer.py
@@ -0,0 +1,28 @@
+import cv2
+print(cv2.__version__)
+
+
+# Cam properties
+fps = 30.
+frame_width = 1920
+frame_height = 1080
+
+
+
+dispW=640
+dispH=480
+flip=2
+#Uncomment These next Two Line for Pi Camera
+camSet = ('videotestsrc ! videoconvert ! appsink')
+          
+cam= cv2.VideoCapture(camSet)
+ 
+
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('nanoCam',frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
