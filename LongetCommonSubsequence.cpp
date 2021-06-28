4c50fc121e661ddfde6811559738b5c71060498e
PhysicsX <ulasdikme@gmail.com>
2021-06-28 22:12:57 +0200

    Longest common subsequence question add

--- LongetCommonSubsequence.cpp
+++ LongetCommonSubsequence.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+//A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements. Longest common subsequence (LCS) of 2 sequences is a subsequence, with maximal length, which is common to both the sequences.
+
+
 using namespace std;
 
 using iter = string::const_iterator;
