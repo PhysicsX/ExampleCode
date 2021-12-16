b76eccd97928ecd795dadc3941be22c6a6acb4fe
physicsX <ulasdikme@gmail.com>
2021-12-16 23:01:55 +0100

    Update dynamic LCS example

--- Cpp/LongestCommonSubsequence.cpp
+++ Cpp/LongestCommonSubsequence.cpp
@@ -3,15 +3,10 @@
 
 //A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements. Longest common subsequence (LCS) of 2 sequences is a subsequence, with maximal length, which is common to both the sequences.
 
+using iter = std::string::const_iterator;
 
-using namespace std;
-
-using iter = string::const_iterator;
-
-// global vector which is used for memorization
-std::vector<std::vector<int>> vec;
-
-int MaxChildRanges(iter b1, iter e1, iter b2, iter e2)
+int MaxChildRanges(iter b1, iter e1, iter b2, iter e2, 
+const std::vector<std::vector<int>>& vec)
 {
     auto& mem = vec[e1-b1][e2-b2];
     
@@ -21,41 +16,29 @@ int MaxChildRanges(iter b1, iter e1, iter b2, iter e2)
     else if(b1==e1 || b2==e2)
         result = 0;
     else if(*b1 == *b2)
-        result = 1 + MaxChildRanges(next(b1), e1, next(b2), e2);
+        result = 1 + MaxChildRanges(next(b1), e1, next(b2), e2, vec);
     else
-        result = max( MaxChildRanges(next(b1), e1, b2, e2),
-                    MaxChildRanges(b1, e1, next(b2), e2));
+        result = std::max( MaxChildRanges(next(b1), e1, b2, e2, vec),
+                    MaxChildRanges(b1, e1, next(b2), e2, vec));
 
     return result;
 }
 
-int commonChild(const string& s1, const string& s2) {
-
-    // fill the vector
-    for(int i = 0; i < s1.size()+1; i++)
-    {
-        std::vector<int> tmp;
-        for(int j = 0; j < s2.size()+1; j++)
-        {
-            tmp.push_back(-1);
-        }
-        vec.push_back(tmp);
-    }
-    
-    return MaxChildRanges(s1.begin(), s1.end(), s2.begin(), s2.end());
-    
+int commonChild(const std::string& s1, const std::string& s2) 
+{
+    return MaxChildRanges(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    std::vector<std::vector<int>> 
+    (s1.size()+1, std::vector<int>(s2.size()+1,-1)));
 }
 
 int main()
 {
-
     std::string str1 {"SHINCHAN"};
     std::string str2 {"NOHARAAA"};
 
     int result = commonChild(str1, str2);
 
-    cout << result << "\n";
-
+    std::cout << result << "\n";
 
     return 0;
 }
