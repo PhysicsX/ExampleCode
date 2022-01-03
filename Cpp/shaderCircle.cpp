34f8f385bb38824913f9d6cee86a4afd4a05a7f6
PhysicsX <ulasdikme@gmail.com>
2022-01-03 21:07:57 +0100

    Circle with shader example

---
+++ Cpp/shaderCircle.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <GLES2/gl2.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+#define GL_SILENCE_DEPRECATION 1
+//g++ -pthread -o shaderCircle shaderCircle.cpp -lGLU -lGL -lXrandr -lXi -lX11 `pkg-config --static --libs glfw3`
+
+
+// vertex shader source
+
+const GLchar* vertex120 = R"END(
+#version 120
+attribute vec3 inPosition;
+void main()
+{
+    gl_Position = vec4(inPosition,1.f);
+}
+)END";
+
+// fragment shader source
+
+const GLchar* raster120 = R"END(
+#version 120
+uniform vec2 res;
+uniform float time;
+void main()
+{
+    vec2 centerPoint = res/4.f;
+    
+    vec2 currentPoint = gl_FragCoord.xy/2.f;
+    
+    if (length(currentPoint - centerPoint) < 100.f) {
+        gl_FragColor = vec4(1,1,1,1);
+    } else {
+        gl_FragColor = vec4(0,0,0,1);
+    }
+}
+)END";
+
+int main()
+{
+    // -------------- init
+    
+    GLFWwindow * window;
+    
+    if (!glfwInit()) {
+        std::cout << "Init error";
+        return -1;
+    }
+    
+    window = glfwCreateWindow(600,600,"Hello",0,0);
+    
+    if (!window) {
+        std::cout << "Window creation error";
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    std::cout << "Init :: checking OpenGL version:\n";
+    const unsigned char * msg;
+    msg = glGetString(GL_VERSION);
+    std::cout << msg << "\n Shader language version: \n";
+    msg = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    std::cout << msg << "\n";
+    
+    const char* source;
+    GLint compilationStatus;
+    
+    // ------------- VERTEX SHADER
+    
+    source = vertex120;
+    
+    GLuint shaderVertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(shaderVertex,1,&source,0);
+    glCompileShader(shaderVertex);
+    
+    glGetShaderiv(shaderVertex, GL_COMPILE_STATUS, &compilationStatus);
+    if (compilationStatus == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderVertex, sizeof(messages), 0, &messages[0]); std::cout << messages;
+        exit(1);
+    }
+    
+    // ---------- FRAGMENT SHADER
+    
+    source = raster120;
+    
+    GLuint shaderFragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(shaderFragment,1,&source,0);
+    glCompileShader(shaderFragment);
+    
+    glGetShaderiv(shaderFragment, GL_COMPILE_STATUS, &compilationStatus);
+    if (compilationStatus == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderFragment, sizeof(messages), 0, &messages[0]); std::cout << messages;
+        exit(1);
+    }
+    
+    // ------------- SHADER PROGRAM
+    
+    GLint linkStatus;
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,shaderVertex);
+    glAttachShader(shaderProgram,shaderFragment);
+    glLinkProgram(shaderProgram);
+    
+    glGetProgramiv(shaderProgram,GL_LINK_STATUS,&linkStatus);
+    if (linkStatus == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(shaderProgram,sizeof(messages),0,&messages[0]);
+        std::cout << messages;
+        exit(1);
+    }
+    
+    glUseProgram(shaderProgram);
+    
+    // ---------------- VBOs
+    
+    GLfloat positions[] = {
+        -1, -1, 0,
+        -1,  1, 0,
+         1, -1, 0,
+         1, -1, 0,
+        -1,  1, 0,
+         1,  1, 0
+    };
+    
+    GLuint positionsData;
+    glGenBuffers(1, &positionsData);
+    glBindBuffer(GL_ARRAY_BUFFER,positionsData);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    
+    // ----------------- attributes
+    GLuint attribPosition;
+    
+    attribPosition = glGetAttribLocation(shaderProgram, "inPosition");
+    glEnableVertexAttribArray(attribPosition);
+    glBindBuffer(GL_ARRAY_BUFFER, positionsData);
+    glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    GLuint uniformRes;
+    uniformRes = glGetUniformLocation(shaderProgram, "res");
+    glUniform2f(uniformRes, 600.f, 600.f);
+    
+    GLuint uniformTime;
+    uniformTime = glGetUniformLocation(shaderProgram, "time");
+    
+    // ----------------- render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0,0,0,0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        float time = glfwGetTime();
+        glUniform1f(uniformTime, time);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+}
