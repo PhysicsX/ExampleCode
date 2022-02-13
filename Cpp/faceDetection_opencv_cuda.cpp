d668f036d9e001ba842defd18a8e59ec5e61f99e
PhysicsX <ulasdikme@gmail.com>
2022-02-13 22:28:40 +0100

    Correct compilation command

--- Cpp/faceDetection_opencv_cuda.cpp
+++ Cpp/faceDetection_opencv_cuda.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/cudaobjdetect.hpp>
 #include <opencv2/cudaimgproc.hpp>
 
-// g++ -I /usr/local/include/opencv4 example.cpp -o example `pkg-config opencv4 --cflags --libs`
+// g++ face_detection_opencv_cuda.cpp -o faceDetectionOpencvCuda `pkg-config opencv4 --cflags --libs`
 
 using namespace cv;
 using namespace std;
@@ -39,24 +39,29 @@ int main() {
         return -1;
     }
     std::vector<cv::Rect> h_found;
-    cv::Ptr<cv::cuda::CascadeClassifier> cascade = cv::cuda::CascadeClassifier::create("/home/ulas/haarcascade_frontalface_default.xml");
+    cv::Ptr<cv::cuda::CascadeClassifier> cascade = cv::cuda::CascadeClassifier::create("haarcascade_frontalface_default.xml");
     cv::cuda::GpuMat d_frame, d_gray, d_found;
     while(1)
     {
-	Mat frame;
+	    Mat frame;
+        
         if ( !cap.read(frame) ) {
             cerr << "Can not read frame from webcam";
             return -1;
         }
+
         d_frame.upload(frame);
         cv::cuda::cvtColor(d_frame, d_gray, cv::COLOR_BGR2GRAY);
         cascade->detectMultiScale(d_gray, d_found);
-	cascade->convert(d_found, h_found);
+	    cascade->convert(d_found, h_found);
+
 		for(int i = 0; i < h_found.size(); ++i)
 		{
               rectangle(frame, h_found[i], Scalar(0,255,255), 5);
 		}
+
         imshow("Result", frame);
+
         if (waitKey(1) == 'q') {
             break;
         }
