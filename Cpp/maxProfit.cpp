f33c7cbf343af355150cfc713c749b9332671cca
physicsX <ulasdikme@gmail.com>
2022-06-15 22:45:30 +0200

    Update max profit example

--- Cpp/maxProfit.cpp
+++ Cpp/maxProfit.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <bits/stdc++.h>
 
 /*
 121. Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
 */
 
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
         
-        int result {0};
-        int currentMax {0};
+        int maxPro{0}, mn{INT_MAX};
         
-        for(int i {0}; i < prices.size(); i++)
-        {
-            int diff = prices[i] - prices[i-1];
-            if(diff == 0) continue;
-            currentMax = std::max(diff + currentMax, diff);
-            result = std::max(result, currentMax);
+        for(size_t i{0};i<prices.size();i++)
+        { 
+            mn=std::min(mn,prices[i]);
+            maxPro=std::max(maxPro,prices[i]-mn);
         }
-        return result;
+        return maxPro;
     }
 };
 
@@ -28,8 +33,8 @@ int main()
 {
     std::vector<int> prices {7,1,5,3,6,4};
     std::cout<<Solution().maxProfit(prices);
-    std::cout<<std::endl;
+    std::cout<<std::endl; // 5
     std::vector<int> prices2 {1,2};
-    std::cout<<Solution().maxProfit(prices2);
+    std::cout<<Solution().maxProfit(prices2); // 1
 
 }
