c11c8ca9ff46d357d3ab87a9e6ee10dfd61089d6
physicsX <ulasdikme@gmail.com>
2022-06-17 23:09:42 +0200

    Update max diff example

--- Cpp/maximumDifference.cpp
+++ Cpp/maximumDifference.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 // Given a 0-indexed integer array nums of size n, 
 // find the maximum difference between nums[i] and nums[j] 
@@ -22,11 +25,55 @@ public:
         return resultMax < 0 ? -1 : resultMax;
     }
 };
+
+template <
+    class result_t   = std::chrono::microseconds,
+    class clock_t    = std::chrono::steady_clock,
+    class duration_t = std::chrono::milliseconds
+>
+auto since(std::chrono::time_point<clock_t, duration_t> const& start)
+{
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
+}
  
 int main()
-{
-    std::vector<int> vec {80, 2, 6, 3, 100};
+{    
+
+    std::vector<int> vec { 1, 3, 4, 6, 2, 5};
     std::cout << "Maximum difference is " << Solution().maximumDifference(vec);
- 
-  return 0;
+    std::cout<<std::endl;
+
+    // generate vector with 0 - 9999
+    std::vector<int> v;
+    for(int i{0}; i<10000; i++)
+    {
+        v.push_back(i);    
+    }
+    
+    // with kadane's algorithm o(n)
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(v), std::end(v), rng);
+
+    std::cout << "Maximum difference is ";
+    auto start = std::chrono::steady_clock::now();
+    int res = Solution().maximumDifference(v);
+    std::cout << "Elapsed(us)=" << since(start).count() << std::endl;
+    std::cout << res;
+    std::cout<<std::endl;
+    
+    
+    // with (n2)
+    int result = 0;
+    start = std::chrono::steady_clock::now();
+    for(size_t i{0}; i<v.size(); i++)
+    {
+        for(size_t j{0}; j<v.size(); j++)
+        {
+            result = std::max(result, v[j] - v[i]);
+        }
+    }
+    std::cout << "Elapsed(us)=" << since(start).count() << std::endl;
+    std::cout<<result<<std::endl;
+
+    return 0;
 }
