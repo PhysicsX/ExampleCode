511f6bd12937b2c2afd080b7ce5141733953edbf
physicsX <ulasdikme@gmail.com>
2022-08-23 21:30:36 +0200

    Update min swap example with deque

--- Cpp/minimumSwap.cpp
+++ Cpp/minimumSwap.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
+#include <deque>
 
 // to order the array/vector in descending order calculate necessary minimum swap number
 
 int numberOfSwap(const std::vector<int>& vec)
 {
-    int result{0};
-    size_t size{vec.size()};
-    
+    unsigned int result{0};
+    const size_t size{vec.size()};
+
     std::vector<std::pair<int, int>> p(size);
-    for(size_t i=0; i<size; i++)
+    for(size_t i{0}; i<size; i++)
     {
         p[i].first = vec[i];
         p[i].second = i;
     }
 
+    // for descending order
     std::sort(p.begin(), p.end(),
         [](const std::pair<int,int> &p1, const std::pair<int,int> &p2){
             return (p1.first > p2.first);    
         });
 
-    // keep visited nodes in another vector
-    std::vector<bool> vis(size, false);
-    
-    // find cycle in array
-    // (edge number of the cycle - 1) is the minimum operation number
-    for(size_t i=0; i<size; i++)
+    // keep visited nodes in deque
+    std::deque<bool> visited(size, false);
+
+    // find cycle in vector/array
+    // (edge number of the cycle - 1) is the minimum swap number
+    // if there is a back edge that means there is a cycle in the directed graph
+    for(size_t i{0}; i<size; i++)
     {
-        if(vis[i] || p[i].second == (int)i)
+        if(visited[i] || p[i].second == (int)i)
             continue;
             
         int edgeNumber{0};
         int j = i;
-        while(!vis[j])
+        while(!visited[j])
         {
-            vis[j] = true;
+            visited[j] = true;
             j = p[j].second;
             edgeNumber++;
         }
