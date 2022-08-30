0d1753dc371a055a1dee777a87c2d356d39115b1
physicsX <ulasdikme@gmail.com>
2022-08-30 20:25:16 +0200

    Refactor binary search example

--- Cpp/binarySearch.cpp
+++ Cpp/binarySearch.cpp
@@ -1,66 +1,53 @@
 #include <algorithm>
-#include <ctime>
 #include <iostream>
+#include <iterator>
 #include <random>
 #include <vector>
-#include <unordered_map>
 #include <chrono>
-
-template <typename C>
-void print(const C& container)
-{
-    for(const auto& val: container)
-        std::cout<<val<<' ';
-    std::cout<<'\n';
-}
 
 int main()
 {
-    std::vector<int> numbers(100);
-    std::generate(numbers.begin(), numbers.end(),[](){
+    size_t size {1000};
+
+    std::vector<int> numbers(size);
+    std::generate(numbers.begin(), numbers.end(),[size](){
 
         static std::mt19937 rng(std::time(nullptr));
-        return std::uniform_int_distribution<>(1,100)(rng);
+        return std::uniform_int_distribution<>(1,size)(rng);
 
     });
 
-    std::cout<<"Here are the sorted numbers:\n";
-    print(numbers);
+    std::cout<<"Numbers:\n";
 
-    std::unordered_map<int,int> map;
-    for(int i = 0; i<numbers.size();i++)
-        map[numbers[i]]++;
+    std::copy(numbers.begin(), numbers.end(),
+              std::ostream_iterator<int>(std::cout, " "));
+    std::cout<<std::endl;
 
-    std::cout<<"Map size"<<map.size()<<"\n";
+    std::sort(numbers.begin(), numbers.end());
+    numbers.erase(std::unique( numbers.begin(), numbers.end()), numbers.end());
 
-    std::vector<int> vec;
-    
-    for(auto it = map.begin(); it != map.end(); ++it)
-        if(it->second > 0)
-            vec.push_back(it->first);
+    std::cout<<"After sort and removed duplicates\n";
+    std::copy(numbers.begin(), numbers.end(),
+              std::ostream_iterator<int>(std::cout, " "));
+    std::cout<<std::endl;
 
-    print(vec);
-
-    std::cout<<"After sorting\n";
-    std::sort(vec.begin(), vec.end());
-    print(vec);
-
-    int target = 55;
-    int left = 0;
-    int right = vec.size()-1;
+    const auto target {55}; 
+    auto left {0};
+    auto right {numbers.size()-1};
 
     int result = -1;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     while(left <= right)
     {
-        int mid = (left+right)/2;
-        if(vec[mid]>target)
+        const auto mid {(left+right)/2};
+        if(numbers[mid]>target)
             right = mid - 1;
-        else if(vec[mid]<target)
+        else if(numbers[mid]<target)
             left = mid + 1;
         else { result = mid; break; }
-
     }
+
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout<<"Time difference = "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"[us]"<<std::endl;
     std::cout<<"Time difference = "<<std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()<<"[ns]"<<std::endl;
@@ -69,23 +56,21 @@ int main()
 
     begin = std::chrono::steady_clock::now();
 
-    for(int i=0; i<vec.size(); i++)
-        if(vec[i] == target)
+    for(size_t i {0}; i<numbers.size(); i++)
+        if(numbers[i] == target)
             result = i;
 
     end = std::chrono::steady_clock::now();
     std::cout<<"Time difference = "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<"[us]"<<std::endl;
     std::cout<<"Time difference = "<<std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()<<"[ns]"<<std::endl;
 
-    // what is the time complexity of the binary search
+    // what is the time complexity of the binary search (log(N))
     // question is how many times it is needed to divide N by 2 to get 1. This is actually binary search
     // 1 = N / 2^x
     // 2^x = N
     // log2(2^x) = log2(N)
     // x = log2(N)
     // this means you have to divide log N until you found your element
-
-
 
     return 0;
 }
