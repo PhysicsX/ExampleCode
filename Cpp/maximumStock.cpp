0e1117e6aaaf397b3317777bc6a14d925e3eefa7
physicsX <ulasdikme@gmail.com>
2022-10-24 21:53:20 +0200

    Update maxstock example

--- Cpp/maximumStock.cpp
+++ Cpp/maximumStock.cpp
@@ -6,6 +6,15 @@
 Each day, you can either buy one share of WOT, sell any number of shares
 of WOT that you own, or not make any transaction at all. What is the
 maximum profit you can obtain with an optimum trading strategy?
+
+- For each share profit is independent. Means that for instance
+array = 1,2,100; then for first day buy, second day buy then 
+sell the shares in third day -> (100-2) + (100-1) = 197
+- Iterate reversely
+
+only difference between maxStock and maxProfit is that for maxStock
+profit is added (stock). For maxProfit, it is needed to keep only maximum one
+profit.
 */
 
 class Solution
@@ -13,7 +22,7 @@ class Solution
     public:
         long maxStock(const std::vector<int>& prices)
         {
-            const int size = prices.size();
+            const size_t size {prices.size()};
             long profit {0};
             int peak {0};
            
@@ -28,10 +37,32 @@ class Solution
             return profit;
         }
 
+        // 121. Best Time to Buy and Sell Stock - leetcode
+        long maxProfit(std::vector<int>& prices) {
+                    
+        const size_t size {prices.size()};
+        int profit {0};
+        int peak {0};
+        
+        for(int i = size-1; i>-1; i--)
+        {
+            if(prices[i] >= peak)
+                peak = prices[i];
+            
+            profit = std::max(profit, peak - prices[i]);
+        }
+        
+        return profit;
+        
+    }
+
 };
 
 int main()
 {
     std::vector<int> stocks{1,2,100};
     std::cout<<Solution().maxStock(stocks);
+    std::endl(std::cout);
+    std::cout<<Solution().maxProfit(stocks);
+
 }
