a611d5addd4ac4266928d4d45ba62f45c83cc4ab
physicsX <ulasdikme@gmail.com>
2022-12-25 23:03:35 +0100

    Update stock pair 2 example for unordered set

--- Cpp/stockPair2.cpp
+++ Cpp/stockPair2.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
+#include <unordered_set>
 #include <vector>
-#include <unordered_map>
 
+// Function to find the number of distinct pairs of stocks that sum to the target profit
 class Solution
 {
-public:
-    int stockPairs(std::vector<int> stocksProfit, 
-                   long target)
-    {
-        int result {0};
-        std::unordered_map<int, int> m;
-        for(size_t i{0}; i<stocksProfit.size(); i++)
-        {
-                m[stocksProfit[i]] ++;
-        }
+    public:
+        int stockPairs(std::vector<int>& profits, int target) {
+            // Initialize a hash set to store the profits that have been seen
+            std::unordered_set<int> seen;
 
-        for(const auto& x : m)
-        {
-            const long diff {target - x.first};
-            if(m.find(diff) != m.end())
-            {
-                if(m[diff] != 0 && 
-                   m[x.first] != 0)
-                {
-                    result ++;
-                    m[diff] --;
-                    m[x.first] --;
+            // Initialize a counter to keep track of the number of pairs
+            int count = 0;
+
+            // Iterate through the array of profits
+            for (int profit : profits) {
+                // Check if the target profit minus the current profit has been seen before
+                if (seen.count(target - profit)) {
+                    // If it has, increment the counter
+                    count++;
                 }
-            }
+                // Add the current profit to the hash set
+                seen.insert(profit);
+                }
+
+            // Return the number of pairs found
+            return count;
         }
-        return result;
-    }
 };
 
-int main()
-{
-    std::vector<int> vec {5, 7, 9, 9, 13, 11, 6, 6, 3, 3};
+int main() {
+    // Define the array of profits and the target profit
+    std::vector<int> profits = {5, 7, 9, 9, 13, 11, 6, 6, 3, 3};
     int target = 12;
 
-    std::cout<<Solution{}.stockPairs(vec, target);
-
-    return 0;
+    std::cout<<Solution{}.stockPairs(profits, target);
 }
