5359cb269c640be127fe012dc1451001ac8766cc
physicsX <ulasdikme@gmail.com>
2023-02-02 21:22:15 +0100

    Brute force example

--- Cpp/kokoEatingBananas.cpp
+++ Cpp/kokoEatingBananas.cpp
@@ -39,13 +39,50 @@ public:
         }
 
         return l;
-    }    
+    }   
+    int minEatingSpeedBruteForce(const std::vector<int>& piles, int target) {
+    
+     for(unsigned int j = 1; j <= *max_element(piles.begin(),piles.end()); j++)
+     {
+         long long hours = 0;
+
+         for(unsigned int i = 0; i < piles.size(); i++)
+         {
+            const auto& currPile = piles[i];
+
+            if(currPile > j)
+            {
+                hours += (currPile / j);   
+                if((currPile % j) != 0) hours++;
+            }
+            else if(currPile <= j)
+            {
+                hours ++;
+            }
+         }
+
+         if(hours == target)
+         {
+             return j;
+         }
+
+         hours = 0;
+    }
+     return 0;
+
+    }
 };
 
 int main()
 {
 	std::vector<int> vec {30,11,23,4,20};
-	std::cout<<Solution{}.findSuitableHour(vec, 6)<<std::endl;
+	std::cout<<Solution{}.minEatingSpeed(vec, 6)<<std::endl;
+	
+	std::cout<<Solution{}.minEatingSpeedBruteForce(vec, 6)<<std::endl;
+
+        // currPile = currPile - (j*(currPile / j)); ==== currPile = (currPile%j)
+	// time exceeded
+        std::cout<<Solution{}.minEatingSpeedBruteForce(std::vector<int>{312884470}, 312884469)<<std::endl;
 }
 
 
