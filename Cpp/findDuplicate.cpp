18060163f475aaf54b489c00a25d9920388969a3
physicsX <ulasdikme@gmail.com>
2023-02-11 19:02:13 +0100

    Add explanation for findDupliacte

--- Cpp/findDuplicate.cpp
+++ Cpp/findDuplicate.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <vector>
 
+
+/*
+ * It is like finding a start point of circle in linked list
+ * Start point of the circle is the first repeated number
+ * Lets define array like linked list with using index 
+ * 1 3 4 2 2 (vector itself)
+ * 0 1 2 3 4 (index)
+ *
+ * 0 goes to 1, 1 goies to 3, 3 goes to 2,
+ * 0 -> 1 -> 3 -> 2 -> 4
+ * 4 -> 2, as it is seen there is a loop between 2 and 4.
+ * And the 2 is the start point of the circle which is
+ * first repeated number in the array/vector.
+ *
+ *
+*/
+
 class Solution {
 public:
     int findDuplicate(std::vector<int>& nums) {
