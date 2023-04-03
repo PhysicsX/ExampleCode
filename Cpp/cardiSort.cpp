82ca3e16873d9ea3e286a246230368ca647614a8
PhysicsX <ulasdikme@gmail.com>
2023-04-03 22:16:08 +0200

    Solution with multimap

--- Cpp/cardiSort.cpp
+++ Cpp/cardiSort.cpp
@@ -22,6 +22,7 @@ class Solution
             multiset<pair<int, int>, ValueComparator> sorted_set(m.begin(), m.end());
 
             vector<int> result;
+            result.reserve(sorted_set.size());
 
             for(const auto& pair : sorted_set)
             {
@@ -47,6 +48,52 @@ class Solution
                             const pair<int, int>& rhs) const {
                 return lhs.second < rhs.second;
             }
+        };
+};
+
+class SolutionWithMultiMap
+{
+    public:
+        vector<int> cardiSort(vector<int> nums)
+        {
+            map<int, int> m;
+            
+            for(int i=0; i<nums.size(); i++)
+            {
+                m[nums[i]] = countOnes(nums[i]);
+            }
+            
+            multimap<int, ValueComparator> sorted_set;
+            
+            for (const auto& kv : m) {
+                sorted_set.insert(std::make_pair(kv.second, ValueComparator(kv.first)));
+            }
+
+            vector<int> result;
+
+            for(const auto& pair : sorted_set)
+            {
+                result.push_back(pair.second.value);
+            }
+
+            return result;
+        }
+
+    private:
+        int countOnes(int n) {
+            int count = 0;
+            while (n) {
+                count += n & 1;
+                n >>= 1;
+            }
+            return count;
+        }
+
+        // Comparator for sorting by value
+        class ValueComparator {
+        public:
+            int value;
+            ValueComparator(int v) : value(v) {}
         };
 };
 
