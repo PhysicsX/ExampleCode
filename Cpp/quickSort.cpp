4d82f2b25b2ef4baccbbcea07ce1d11421dc1e4f
PhysicsX <ulasdikme@gmail.com>
2023-05-07 19:18:48 +0200

    Explanation for quick sort

--- Cpp/quickSort.cpp
+++ Cpp/quickSort.cpp
@@ -2,6 +2,17 @@
 #include <vector>
 #include <algorithm>
 
+/*
+    idea is that choose a pivot (can be done randomly or choosing from left/right)
+    define left and right element. In the beginning left is 0 and right is size of the vector
+    Iterate through left to right. according to pivot elements value, swap the iterated element such that
+    if it is greater than the pivot value the swap with index which can be choosen from left or right.
+    The idea is to collect the elements which are greater than the pivot to the right and smaller than to 
+    left.
+    Recursion can be used to make continue this process. Pivot should be replacted at the end when iteration
+    is finished for each partioning.
+*/
+
 int partition(std::vector<int>& vec, int left, int right)
 {
     int pivot = vec[left];
